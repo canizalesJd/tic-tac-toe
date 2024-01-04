e079158670707dc3d75389ca5d7e6383010509f6
Jose Canizales <canizalesjd@gmail.com>
2024-01-04 00:05:05 -0600

    Initial UI changes

--- js/main.js
+++ js/main.js
@@ -4,25 +4,28 @@ function BoardCreation() {
 	const rows = 3;
 	const board = [];
 	// Creating 2D array
+	const boardDiv = document.createElement("div");
+	boardDiv.classList.add("board");
+	const gameContainer = document.querySelector(".game-container");
+	gameContainer.appendChild(boardDiv);
 	for (let i = 0; i < columns; i++) {
 		// row
 		board[i] = [];
 		for (let j = 0; j < rows; j++) {
 			// cell
+			const boardCellDiv = document.createElement("div");
+			boardCellDiv.classList.add("board-cell");
+			boardDiv.appendChild(boardCellDiv);
 			board[i][j] = "";
 		}
 	}
 	return { board, columns, rows };
 }
 
-// Creating a function to select a cell
-function SelectCell(columns = 3, rows = 3) {
-	let cell = prompt("Select a cell");
-	while (isNaN(cell) || cell === "" || cell < 1 || cell > rows * columns) {
-		cell = prompt("Invalid input, select a cell again");
-	}
-	const row = Math.floor((cell - 1) / columns);
-	const column = (cell - 1) % columns;
+// Creating a function to get the row and column from a cell
+function TransformCellNumber(cellNumber, columns = 3) {
+	const row = Math.floor((cellNumber - 1) / columns);
+	const column = (cellNumber - 1) % columns;
 	return { row, column };
 }
 
@@ -38,32 +41,37 @@ function CheckStatus(board, player1, player2) {
 		// Check rows
 		if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
 			if (board[i][0] === player1) {
+				alert("Player 1 wins");
 				return { status: "win", player: player1 };
 			} else if (board[i][0] === player2) {
+				alert("Player O wins");
 				return { status: "win", player: player2 };
 			}
 		}
 		// Check columns
 		if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
 			if (board[0][i] === player1) {
+				alert("Player 1 wins");
 				return { status: "win", player: player1 };
 			} else if (board[0][i] === player2) {
-				console.log("Player 2 wins");
+				alert("Player O wins");
 				return { status: "win", player: player2 };
 			}
 		}
 		// Check diagonals
 		if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
 			if (board[0][0] === player1) {
+				alert("Player X wins");
 				return { status: "win", player: player1 };
 			} else if (board[0][0] === player2) {
+				alert("Player O wins");
 				return { status: "win", player: player2 };
 			}
 		}
 	}
 	// If no winner, check for draw
 	if (board.every((row) => row.every((cell) => cell !== ""))) {
-		console.log("Draw");
+		alert("Draw");
 		return { status: "draw", player: null };
 	}
 	// If no winner or draw, continue game
@@ -71,7 +79,7 @@ function CheckStatus(board, player1, player2) {
 }
 
 function Game() {
-	const { rows, columns, board } = BoardCreation();
+	const { board } = BoardCreation();
 	// Declare players
 	const player1 = "X";
 	const player2 = "O";
@@ -79,30 +87,48 @@ function Game() {
 	let currentPlayer = player1;
 	// Repeat rows * columns times
 	let round = 1;
-	while (round <= rows * columns) {
-		// If odd X, else O
-		if (round % 2 !== 0) {
-			currentPlayer = player1;
-		} else {
-			currentPlayer = player2;
-		}
-		// Getting the row and column
-		const { row, column } = SelectCell(columns, rows);
-		if (board[row][column] === "") {
-			ModifyBoard(board, row, column, currentPlayer);
-			round++;
-		} else {
-			alert("Cell not empty, select a different one");
-		}
-		const { status, player } = CheckStatus(board, player1, player2);
-		if (status === "win") {
-			console.log(`Player ${player} wins1`);
-			return `Player ${player} wins`;
-		}
-		if (status === "draw") {
-			return `It's a draw`;
-		}
-	}
+	// Select Sell number
+	const cellsDiv = document.querySelectorAll(".board-cell");
+	cellsDiv.forEach((cell, cellNumber) => {
+		cell.addEventListener("click", () => {
+			console.log(round);
+			// If odd X, else O
+			if (round % 2 !== 0) {
+				currentPlayer = player1;
+			} else {
+				currentPlayer = player2;
+			}
+			// Update the cell with the selection
+			if (cell.textContent === "") {
+				if (currentPlayer === player1) {
+					cell.textContent = "X";
+					currentPlayer = player2;
+				} else {
+					cell.textContent = "O";
+					currentPlayer = player1;
+				}
+			}
+			const currentPlayerDiv = document.querySelector(".current-player");
+			currentPlayerDiv.textContent = `Current player: ${currentPlayer}`;
+			// Getting the row and column
+			const { row, column } = TransformCellNumber(cellNumber);
+			if (board[row][column] === "") {
+				ModifyBoard(board, row, column, currentPlayer);
+				console.log(board);
+				round++;
+			} else {
+				alert("Cell not empty, select a different one");
+			}
+			const { status, player } = CheckStatus(board, player1, player2);
+			if (status === "win") {
+				console.log(`Player ${player} wins1`);
+				return `Player ${player} wins`;
+			}
+			if (status === "draw") {
+				return `It's a draw`;
+			}
+		});
+	});
 }
 
 // Call game function
