1bafe4302427f1220684d04ef9e47c39d0eaadc3
Jose Canizales <canizalesjd@gmail.com>
2024-01-03 21:33:42 -0600

    Initial game functions

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// Creating a 3 by 3 board for the tic tac toe
+function BoardCreation() {
+	const columns = 3;
+	const rows = 3;
+	const board = [];
+	// Creating 2D array
+	for (let i = 0; i < columns; i++) {
+		// row
+		board[i] = [];
+		for (let j = 0; j < rows; j++) {
+			// cell
+			board[i][j] = "";
+		}
+	}
+	return { board, columns, rows };
+}
+
+// Creating a function to select a cell
+function SelectCell(columns = 3) {
+	const cell = prompt("Select a cell");
+	const row = Math.floor((cell - 1) / columns);
+	4;
+	const column = (cell - 1) % columns;
+	return { row, column };
+}
+
+function Game() {
+	const { rows, columns, board } = BoardCreation();
+	// Declare players
+	const player1 = "X";
+	const player2 = "O";
+	// Initialize current player to X
+	let currentPlayer = player1;
+	// Repeat rows * columns times
+	let round = 1;
+	while (round <= rows * columns) {
+		// If odd X, else O
+		if (round % 2 !== 0) {
+			currentPlayer = player1;
+		} else {
+			currentPlayer = player2;
+		}
+		// Getting the row and column
+		const { row, column } = SelectCell(columns);
+		if (board[row][column] === "") {
+			ModifyBoard(board, row, column, currentPlayer);
+			round++;
+		} else {
+			console.log("Cell not empty, select a different one");
+		}
+	}
+	console.log(board);
+}
+
+// Function to modify the board
+function ModifyBoard(board, row, column, player) {
+	board[row][column] = player;
+	return board;
+}
+
+Game();
